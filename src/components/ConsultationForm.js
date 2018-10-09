34635c3846bd5f6d8f274cb47e5ca115ed1f20d6
gavin-love <1co.loco@oprotonmail.ch>
2018-10-09 15:09:57 -0600

    Deploy to heroku

--- src/components/ConsultationForm.js
+++ src/components/ConsultationForm.js
@@ -13,16 +13,44 @@ class ConsultationForm extends Component {
 
   }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   onSubmit = () => this.props.history.push('/confirmation');
 
   render() {
+    const disabled = !this.state.email && !this.state.name
+      && !this.state.phoneNumber
+      || !this.state.email || !this.state.name
+      || !this.state.phoneNumber ? true : false;
+
     return (
       <form onSubmit={this.onSubmit}>
         <p>Enter your information and we will reach out to schedule a consultation</p>
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="phone number" />
-        <button>Submit</button>
+        <input
+          onChange={this.handleChange}
+          value={this.state.name}
+          name="name" type="text"
+          placeholder="name"
+        />
+        <input
+          onChange={this.handleChange}
+          value={this.state.email}
+          name="email" type="text"
+          placeholder="email"
+        />
+        <input
+          onChange={this.handleChange}
+          value={this.state.phoneNumber}
+          name="phoneNumber" type="text"
+          placeholder="phone number"
+        />
+        <button disabled={disabled}>Submit</button>
       </form>
     )
   }
